16b7e8a9e65ead48ef164f877893d8bc15cb7473
SasaDasTec <samueltolentinoalves@gmail.com>
2024-12-09 14:16:46 -0300

    firts commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function Hamburg() {
+    document.querySelector('.dropdown').style.transform = 'translateY(0)';
+}
+
+function cancel() {
+    document.querySelector('.dropdown').style.transform = 'translateY(-500px)';
+}
